6b6f55419c04049d57fce77e7790eed0538bbf9a
Maria Lamardo <marialamardo@outlook.com>
2018-02-05 19:51:25 -0500

    edit JS

--- js/app.js
+++ js/app.js
@@ -41,19 +41,28 @@ topics.display = function () {
 	topics.Beginner.forEach(function(topic){ 
         $("#first").append(HTMLprojectStart);
         var replaceChars={ "%link%":topic.link, "%img%": topic.image, "%title%": topic.title };
-        var formattedprojectTitle = HTMLprojectTitle.replace(/%link%|%img%|%title%/g,function(match) {return replaceChars[match];})
+		var formattedprojectTitle = HTMLprojectTitle.replace(/%link%|%img%|%title%/g,
+			function(match) {
+				return replaceChars[match];
+			});
 		$("#first li:last").append(formattedprojectTitle);	
 	});
 	topics.Intermediate.forEach(function(topic){ 
         $("#second").append(HTMLprojectStart);
         var replaceChars={ "%link%":topic.link, "%img%": topic.image, "%title%": topic.title };
-        var formattedprojectTitle = HTMLprojectTitle.replace(/%link%|%img%|%title%/g,function(match) {return replaceChars[match];})
+        var formattedprojectTitle = HTMLprojectTitle.replace(/%link%|%img%|%title%/g,
+			function(match) {
+				return replaceChars[match];
+			});
 		$("#second li:last").append(formattedprojectTitle);	
 	});
 	topics.Advanced.forEach(function(topic){ 
         $("#third").append(HTMLprojectStart);
         var replaceChars={ "%link%":topic.link, "%img%": topic.image, "%title%": topic.title };
-        var formattedprojectTitle = HTMLprojectTitle.replace(/%link%|%img%|%title%/g,function(match) {return replaceChars[match];})
+        var formattedprojectTitle = HTMLprojectTitle.replace(/%link%|%img%|%title%/g,
+			function(match) {
+				return replaceChars[match];
+			});
 		$("#third li:last").append(formattedprojectTitle);	
 	});
 };
