7ac75b3294c11874241456e67cf1ae045fc9c27e
Maria Lamardo <marialamardo@outlook.com>
2018-02-28 15:42:20 -0500

    meaningful variabke names and comments

--- js/app.js
+++ js/app.js
@@ -3,39 +3,48 @@ These are HTML strings. JavaScript functions
 replace the %data% placeholder text you see in them.
 */
 
-// Projects
-var HTMLprojectStart = '<li></li>';
-var HTMLprojectTitle = '<a href="%link%"><img src="%img%"/><span class="text-content"><span>%title%</span></span></a>';
+// Create list with content
+const listedResources = '<li></li>';
+const resourcesContent = '<a href="%link%"><img src="%img%"/><span class="text-content"><span>%title%</span></span></a>';
 
 
 //Display content from topic
 topics.display = function () {
+
+	//displays CSS topic resources
 	topics.CSS.forEach(function(topic){ 
-        $("#first").append(HTMLprojectStart);
-        var replaceChars={ "%link%":topic.link, "%img%": topic.image, "%title%": topic.title };
-		var formattedprojectTitle = HTMLprojectTitle.replace(/%link%|%img%|%title%/g,
+		$("#first").append(listedResources);
+		//Match %data% with object
+		const replaceChars={ "%link%":topic.link, "%img%": topic.image, "%title%": topic.title };
+		//Replace %data% with object informtaion
+		const formattedContent = resourcesContent.replace(/%link%|%img%|%title%/g,
 			function(match) {
 				return replaceChars[match];
 			});
-		$("#first li:last").append(formattedprojectTitle);	
+		//Add content to page
+		$("#first li:last").append(formattedContent);	
 	});
+
+	//displays HTML topic resources
 	topics.HTML.forEach(function(topic){ 
-        $("#second").append(HTMLprojectStart);
-        var replaceChars={ "%link%":topic.link, "%img%": topic.image, "%title%": topic.title };
-        var formattedprojectTitle = HTMLprojectTitle.replace(/%link%|%img%|%title%/g,
+        $("#second").append(listedResources);
+        const replaceChars={ "%link%":topic.link, "%img%": topic.image, "%title%": topic.title };
+        const formattedContent = resourcesContent.replace(/%link%|%img%|%title%/g,
 			function(match) {
 				return replaceChars[match];
 			});
-		$("#second li:last").append(formattedprojectTitle);	
+		$("#second li:last").append(formattedContent);	
 	});
+
+	//displays JS topic resources
 	topics.JS.forEach(function(topic){ 
-        $("#third").append(HTMLprojectStart);
-        var replaceChars={ "%link%":topic.link, "%img%": topic.image, "%title%": topic.title };
-        var formattedprojectTitle = HTMLprojectTitle.replace(/%link%|%img%|%title%/g,
+        $("#third").append(listedResources);
+        const replaceChars={ "%link%":topic.link, "%img%": topic.image, "%title%": topic.title };
+        const formattedContent = resourcesContent.replace(/%link%|%img%|%title%/g,
 			function(match) {
 				return replaceChars[match];
 			});
-		$("#third li:last").append(formattedprojectTitle);	
+		$("#third li:last").append(formattedContent);	
 	});
 };
 topics.display();
