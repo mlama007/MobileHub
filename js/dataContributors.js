bd3d531fd7157edd986639a39f3c3314e4b10d2d
Maria Lamardo <marialamardo@outlook.com>
2018-03-19 18:14:27 -0400

    Toggle Who We Are visibility

--- js/dataContributors.js
+++ js/dataContributors.js
@@ -6,19 +6,25 @@
 
     exports.data = {
 		"Maria Lamardo" : {
+            "img" : "maria.svg",
             "name" : "Maria Lamardo",
-			"title" : ["Front-End Developer", "UI/UX Designer", "BCaBA"],
-			"summary" : "My name is BLAH",
+            "title" : ["Front-End Developer", "UI/UX Designer", "BCaBA"],
+            "intro" : "This goes on front Page.",
+			"summary" : "This goes on front Contributors Page. I'm gonna build me an airport, put my name on it. A-coodle-doodle-doo. A-coodle-doodle-doo. So maybe you could start jete-ing, and stop je-terrorizing me!",
 		},
 		"Varell" : {
+            "img" : "varell.svg",
             "name" : "Varell",
 			"title" : ["Front-End Developer"],
-			"summary" : "My name is BLAH",
+			"intro" : "This goes on front Page.",
+			"summary" : "This goes on front Contributors Page. I'm gonna build me an airport, put my name on it. A-coodle-doodle-doo. A-coodle-doodle-doo. So maybe you could start jete-ing, and stop je-terrorizing me!",
 		},
 		"Jannaee Sick" : {
+            "img" : "jannaee.svg",
             "name" : "Jannaee Sick",
 			"title" : ["Front-End Developer", "UI/UX Designer"],
-			"summary" : "My name is BLAH",
+			"intro" : "This goes on front Page.",
+			"summary" : "This goes on front Contributors Page. I'm gonna build me an airport, put my name on it. A-coodle-doodle-doo. A-coodle-doodle-doo. So maybe you could start jete-ing, and stop je-terrorizing me!",
 		},
     }    
 })();
