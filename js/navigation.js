145441bad7bcae765139c13f726c79eca28396fb
Jannaee <jannaee@gmail.com>
2018-03-28 01:05:04 -0400

    added dropdown sections to the navigation

--- js/navigation.js
+++ js/navigation.js
@@ -43,3 +43,19 @@ function upAction() {
 window.addEventListener("scroll", function() {
     scrollDetect(homeAction, downAction, upAction);
 });
+
+// drop down for main navigation
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
