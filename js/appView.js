8a26bcd5b846b1fd7f73a89573cd37ebb2c77416
vjhawkins <vjhawkins@yahoo.com>
2019-01-08 18:55:58 -0800

    Started Over - Add All event - Add other difficulty event

--- js/appView.js
+++ js/appView.js
@@ -1,99 +1,40 @@
-(function(undefined) {
-    //When Categories are clicked
+(function (undefined) {
+	// appView handles user events	
 
-    //When Boxes are clicked
-    const main = function() {
-        $('.topics li').click(function() {
-			const category = $(this).attr('class');
-		    const removeSelected = $('.boxes').removeClass('selected');
-		    const addContent = $('.topicImg').addClass('content');
-			const removeVisibility= $('.topicImg').removeClass('visible');
-			
-			$('.aboutUs').addClass('NoContent');		
-			$('.All').addClass('ViewContent');
-			$('.All').removeClass('NoContent');
-			
-			// When CSS clicked
-		    if (category === "boxes CSSBox") {
-		    	removeSelected
-		    	$('.CSSBox').addClass('selected');
-		    	addContent
-		    	removeVisibility
-		    	$('.CSS').removeClass('content');
-				$('.CSS').addClass('visible');				
+	var exports = {};
+	window.MobileHub = window.MobileHub || {};
+	window.MobileHub.Events = exports;
+
+	window.onload = function() {
+
+	};
+
+	// All Difficulty filter event
+	document.querySelector('[data-difficulty="All"]').addEventListener('click', function(event) {
+		const element = event.target;
+		Array.from(element.parentElement.children).forEach(elem => {
+			if (elem.getAttribute('data-difficulty') !== 'All') {
+				elem.classList.remove('selected-difficulty');
 			}
-			// When HTML clicked	
-		    else if (category === "boxes HTMLBox") {
-		    	removeSelected
-		    	$('.HTMLBox').addClass('selected');
-		    	addContent
-		    	removeVisibility
-		    	$('.HTML').removeClass('content');
-		    	$('.HTML').addClass('visible');
-			}
-			// When JS clicked	
-		    else if (category === "boxes JSBox") {
-		    	removeSelected
-		    	$('.JSBox').addClass('selected');
-		    	addContent
-		    	removeVisibility
-		    	$('.JS').removeClass('content');
-		    	$('.JS').addClass('visible');
-			}
-			// When Git clicked
-		    else if (category === "boxes GitBox") {
-		    	removeSelected
-		    	$('.GitBox').addClass('selected');
-		    	addContent
-		    	removeVisibility
-		    	$('.Git').removeClass('content');
-		    	$('.Git').addClass('visible');
-			}
-			// When Learning clicked
-		    else if (category === "boxes LearningBox") {
-		    	removeSelected
-		    	$('.LearningBox').addClass('selected');
-		    	addContent
-		    	removeVisibility
-		    	$('.Learning').removeClass('content');
-		    	$('.Learning').addClass('visible');
-			}
-			// When Resources clicked			
-		    else if (category === "boxes ResourcesBox") {
-		    	removeSelected
-		    	$('.ResourcesBox').addClass('selected');
-		    	addContent
-		    	removeVisibility
-		    	$('.Resources').removeClass('content');
-		    	$('.Resources').addClass('visible');
-			}
-			// When Frameworks clicked
-		    else if (category === "boxes FrameworksBox") {
-		    	removeSelected
-		    	$('.FrameworksBox').addClass('selected');
-		    	addContent
-		    	removeVisibility
-		    	$('.Frameworks').removeClass('content');
-		    	$('.Frameworks').addClass('visible');
-			}
-			// When Practice is clicked
-		    else if (category === "boxes PracticeBox") {
-		    	removeSelected
-		    	$('.PracticeBox').addClass('selected');
-		    	addContent
-		    	removeVisibility
-		    	$('.Practice').removeClass('content');
-		    	$('.Practice').addClass('visible');
-			}
-			// When anything else is clicked
-			else{
-				$('.All').removeClass('ViewContent');
-				$('.All').addClass('NoContent');
-				console.log("HERE");
-				$('.aboutUs').removeClass('NoContent');
-			}
-      });
-    }
-    
-    $(document).ready(main);
+		});
+		element.classList.toggle('selected-difficulty');
+
+		if (element.classList.contains('selected-difficulty')) {
+			window.MobileHub.displayAll();
+		}
+		else {
+			// window.MobileHub.removeAll();
+		}
+	});
+
+	// Beginner, Intermediate, Advanced Difficulty filter event
+	Array.from(document.querySelectorAll('[data-difficulty]'))
+	.filter(difficulty => difficulty.getAttribute('data-difficulty') != 'All')
+	.forEach(element => {
+		element.addEventListener('click', event => {
+			document.querySelector('[data-difficulty="All"]').classList.remove('selected-difficulty');
+			event.target.classList.toggle('selected-difficulty');
+		})
+	});
+
 })();
