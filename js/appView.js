529a5e474a9b653de988a3a19630e969ae9ae527
vjhawkins <vjhawkins@yahoo.com>
2019-01-10 23:58:30 -0800

    Update difficulty filters - All difficulty get all data and display it in the UI - All difficulty buttons toggle UI - Replace selected state tracking with data-selected - Added Startup and Tools categories to UI

--- js/appView.js
+++ js/appView.js
@@ -4,37 +4,77 @@
 	var exports = {};
 	window.MobileHub = window.MobileHub || {};
 	window.MobileHub.Events = exports;
-
+	
+	//-- Dependencies Section --//
+	const categories = window.MobileHub.Categories.data;
+	
+	//--- Run when page loads ---//
 	window.onload = function() {
 
 	};
 
-	// All Difficulty filter event
+	//--- Events Section ---//
+
+	// All difficulty filter event
 	document.querySelector('[data-difficulty="All"]').addEventListener('click', function(event) {
 		const element = event.target;
+		// Deselect Beginner, Imtermidate, & Advanced difficulty
 		Array.from(element.parentElement.children).forEach(elem => {
 			if (elem.getAttribute('data-difficulty') !== 'All') {
-				elem.classList.remove('selected-difficulty');
+				elem.dataset.selected = false;
 			}
 		});
-		element.classList.toggle('selected-difficulty');
+		// Toggle All difficulty
+		element.dataset.selected = !(element.dataset.selected === "true");
 
-		if (element.classList.contains('selected-difficulty')) {
+		// .content hides all results
+		const results = document.querySelectorAll('.topicImg');
+		if (element.dataset.selected === 'true') {
+			// Show results
 			window.MobileHub.displayAll();
+			results.forEach(result => result.classList.remove('content'));
+			console.info('[All Show]');
 		}
 		else {
+			// Hide results
 			// window.MobileHub.removeAll();
+			results.forEach(result => result.classList.add('content'));
+			console.info('[All Hide]');
 		}
 	});
 
-	// Beginner, Intermediate, Advanced Difficulty filter event
-	Array.from(document.querySelectorAll('[data-difficulty]'))
+	// Beginner, Intermediate, Advanced difficulty filter event
+	const biaFilterArray = Array.from(document.querySelectorAll('[data-difficulty]'))
+	
+	biaFilterArray
+	// Remove 'All' from list
 	.filter(difficulty => difficulty.getAttribute('data-difficulty') != 'All')
 	.forEach(element => {
+		// Select / Deselect each clicked difficulty
 		element.addEventListener('click', event => {
-			document.querySelector('[data-difficulty="All"]').classList.remove('selected-difficulty');
-			event.target.classList.toggle('selected-difficulty');
+			document.querySelector('[data-difficulty="All"]').dataset.selected = false;
+
+			// Toggle selected state
+			let selected = event.target;
+			selected.dataset.selected = !(selected.dataset.selected === "true");
+
+			// .content hides all results
+			const results = document.querySelectorAll('.topicImg');
+			console.info('[biaFilterArray]', biaFilterArray);
+
+			// Check if any difficulty filter is selected
+			if (biaFilterArray.every(element => {
+				console.info('[selected]', element.dataset);
+				return element.dataset.selected === 'false';
+			})) {
+				// Hide results
+				results.forEach(result => result.classList.add('content'));
+			}
+			else {
+				// Show results
+				results.forEach(result => result.classList.remove('content'));
+			}
 		})
-	});
+	})
 
 })();
